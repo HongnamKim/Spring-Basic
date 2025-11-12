2bc6277e9b63e9222b4cf749c1eb76378cfd2eae
HongnamKim <ghdskawkd@naver.com>
2025-11-12 12:55:17 +0900

    스프링 데이터 JPA, AOP

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,10 +1,7 @@
 package hello.hello_spring;
 
-import hello.hello_spring.repository.JpaMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,25 +10,32 @@ public class SpringConfig {
   //  @Autowired
   //  DataSource dataSource;
 
-  private final DataSource dataSource;
-  private final EntityManager em;
+  //  private final DataSource dataSource;
+  //  private final EntityManager em;
+  //
+  //  public SpringConfig(DataSource dataSource, EntityManager em) {
+  //    this.dataSource = dataSource;
+  //    this.em = em;
+  //  }
 
-  public SpringConfig(DataSource dataSource, EntityManager em) {
-    this.dataSource = dataSource;
-    this.em = em;
+  private final MemberRepository memberRepository;
+
+  public SpringConfig(MemberRepository memberRepository) {
+    this.memberRepository = memberRepository;
   }
 
-  @Bean
-  public MemberRepository memberRepository() {
-    // return new MemoryMemberRepository();
-    // return new JdbcMemberRepository(dataSource);
-    // return new JdbcTemplateMemberRepository(dataSource);
-
-    return new JpaMemberRepository(em);
-  }
+  //  @Bean
+  //  public MemberRepository memberRepository() {
+  //    // return new MemoryMemberRepository();
+  //    // return new JdbcMemberRepository(dataSource);
+  //    // return new JdbcTemplateMemberRepository(dataSource);
+  //
+  //    return new JpaMemberRepository(em);
+  //
+  //  }
 
   @Bean
   public MemberService memberService() {
-    return new MemberService(memberRepository());
+    return new MemberService(memberRepository);
   }
 }
